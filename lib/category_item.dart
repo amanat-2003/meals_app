1d7957a1f7e7adbfd18182687a87032794b74581
amanat-2003 <amanatsinghnain@gmail.com>
2022-09-20 18:43:16 +0530

    lec162 completed

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -17,7 +17,7 @@ class CategoryItem extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            color.withOpacity(0.7),
+            color.withOpacity(0.4),
             color,
           ],
           begin: Alignment.topLeft,
--- lib/main.dart
+++ lib/main.dart
@@ -21,10 +21,6 @@ class MyApp extends StatelessWidget {
             bodyText2: TextStyle(
               color: Color.fromRGBO(20, 51, 51, 1),
             ),
-            // titleLarge: TextStyle(
-            //   fontSize: 24,
-            //   fontFamily: 'RobotoCondensed',
-            // )
             headline6: TextStyle(
               fontSize: 20,
               fontFamily: 'RobotoCondensed',
