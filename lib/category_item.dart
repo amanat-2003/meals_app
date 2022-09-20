1b10cba9daabae142974d9bd007daa45253ebbb4
amanat-2003 <amanatsinghnain@gmail.com>
2022-09-20 19:54:58 +0530

    lec164 completed

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,16 +2,24 @@ import 'package:flutter/material.dart';
 import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem({this.title, this.color});
+  CategoryItem({
+    @required this.id,
+    @required this.title,
+    @required this.color,
+  });
 
   void selectCategory(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
-          return CategoryMealsScreen();
+          return CategoryMealsScreen(
+            categoryId: id,
+            categoryTitle: title,
+          );
         },
       ),
     );
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen({
+    this.categoryId,
+    this.categoryTitle,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(this.categoryTitle),
       ),
       body: Center(
-      child: Text("The Recipies For The Category!"),
-    ),
+        child: Text("The Recipies For The Category!"),
+      ),
     );
-
-    
   }
 }
