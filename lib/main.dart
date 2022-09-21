538485cd3c0f959a34e9553e4b767f5e713a8469
amanat-2003 <amanatsinghnain@gmail.com>
2022-09-21 20:44:09 +0530

    lec173 completed

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,24 @@ class MyApp extends StatelessWidget {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-            bodyText1: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
+              bodyText1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyText2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              headline6: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+              caption: TextStyle(
+                fontSize: 20,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.bold,
+                // fontStyle: FontStyle.italic,
+              ),
             ),
-            bodyText2: TextStyle(
-              color: Color.fromRGBO(20, 51, 51, 1),
-            ),
-            headline6: TextStyle(
-              fontSize: 20,
-              fontFamily: 'RobotoCondensed',
-              fontWeight: FontWeight.bold,
-            )),
       ),
       // home: CategoriesScreen(),
       initialRoute: '/',
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -2,17 +2,95 @@ import 'package:flutter/material.dart';
 
 import '../dummy_data.dart';
 
+Widget buildSectionTitle({BuildContext context, String text}) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    child: Text(
+      text,
+      style: Theme.of(context).textTheme.caption,
+    ),
+  );
+}
+
+Widget buildContainer({BuildContext context, Widget child}) {
+  return Container(
+    height: 150,
+    width: 300,
+    margin: EdgeInsets.all(5),
+    padding: EdgeInsets.symmetric(vertical: 4, horizontal: 5),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      border: Border.all(
+        color: Colors.grey,
+        width: 2,
+      ),
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: child,
+  );
+}
+
 class MealDetailScreen extends StatelessWidget {
   static const routeName = "/meal-detail";
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere(((meal) => meal.id == mealId));
     return Scaffold(
-      appBar: AppBar(title: Text(mealId),),
-      body: Center(
-        child: Text('The Meal! - $mealId'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(selectedMeal.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: double.infinity,
+                child: Image.network(
+                  selectedMeal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              buildSectionTitle(context: context, text: "Ingredients"),
+              buildContainer(
+                context: context,
+                child: ListView.builder(
+                  itemBuilder: (ctx, index) => Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 5,
+                      ),
+                      child: Text(selectedMeal.ingredients[index]),
+                    ),
+                  ),
+                  itemCount: selectedMeal.ingredients.length,
+                ),
+              ),
+              buildSectionTitle(context: context, text: "Steps"),
+              buildContainer(
+                  context: context,
+                  child: ListView.builder(
+                    itemBuilder: ((ctx, index) => Column(
+                          children: [
+                            ListTile(
+                              leading: CircleAvatar(
+                                radius: 18,
+                                child: Text("#${index + 1}"),
+                              ),
+                              title: Text(selectedMeal.steps[index]),
+                            ),
+                            Divider(
+                              thickness: 0.5,
+                            ),
+                          ],
+                        )),
+                    itemCount: selectedMeal.steps.length,
+                  ))
+            ],
+          ),
+        ));
   }
 }
